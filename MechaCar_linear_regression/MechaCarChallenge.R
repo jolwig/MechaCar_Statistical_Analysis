f0403aa731ba5dbf867ffe5ac59578172bf991d8
Jake Olwig <jolwig10@gmail.com>
2023-03-29 19:58:26 -0500

    adding pngs

--- MechaCar_linear_regression/MechaCarChallenge.R
+++ MechaCar_linear_regression/MechaCarChallenge.R
@@ -5,3 +5,9 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 suspension_coil_table <-read.csv(file='Suspension_Coil.csv', check.names=F,stringsAsFactors = F)#import suspension_coil.csv
 total_summary <- suspension_coil_table %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), groups = 'keep') #create summary table for psi
 lot_summary <- suspension_coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), groups = 'keep')#group manufacturing lot by mean median variance and sd
+
+
+t.test(suspension_coil_table$PSI, mu=1500)#t-test for all lots
+t.test(subset(suspension_coil_table$PSI, suspension_coil_table$Manufacturing_Lot == "Lot1"), mu=1500)#t-test for lot 1
+t.test(subset(suspension_coil_table$PSI, suspension_coil_table$Manufacturing_Lot == "Lot2"), mu=1500)#t-test for lot 2
+t.test(subset(suspension_coil_table$PSI, suspension_coil_table$Manufacturing_Lot == "Lot3"), mu=1500)#t-test for lot 3
